bfa3f50c39321458ec0c81b6e31f86cd698ec4bd
Michael Burge <michaelburge@pobox.com>
2017-09-08 13:03:24 -0700

    Successfully inject perft() into Redshift

--- chess.c
+++ chess.c
@@ -1082,8 +1082,15 @@ private move mkPromotion(gamestate g, int from, int piece)
 }
 
 // Public functions
-
-private int num_bits(uint64_t x) { return __builtin_popcountl(x); }
+// we could use __builtin_popcountl, but we'd need -march=native to eliminate library call which is less portable.
+unsigned int popcount64(unsigned long long x)
+{
+    x = (x & 0x5555555555555555ULL) + ((x >> 1) & 0x5555555555555555ULL);
+    x = (x & 0x3333333333333333ULL) + ((x >> 2) & 0x3333333333333333ULL);
+    x = (x & 0x0F0F0F0F0F0F0F0FULL) + ((x >> 4) & 0x0F0F0F0F0F0F0F0FULL);
+    return (x * 0x0101010101010101ULL) >> 56;
+}
+private int num_bits(uint64_t x) { return popcount64(x); }
 
 private int score_pieces(gamestate g)
 {
--- compile.sh
+++ compile.sh
@@ -3,7 +3,6 @@
 g++ -c chess.c \
     -Os \
     -fPIC \
-    -fno-jump-tables \
-    -march=native
+    -fno-jump-tables
 
 bash -c 'cd dump-elf-bytes; stack exec dump-elf-bytes -- ../chess.o > ../shellcode_bytes.py'
